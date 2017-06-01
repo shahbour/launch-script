8fcfe43619e27604df306d56ad4509f3a024d7d4
Ali Shahbour <shahbour@gmail.com>
2017-06-01 12:50:54 +0300

    initial commit

---
+++ src/main/java/com/shahbour/LaunchScriptApplication.java
@@ -0,0 +1,12 @@
+package com.shahbour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchScriptApplication.class, args);
+	}
+}
